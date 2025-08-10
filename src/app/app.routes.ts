90b488a54d4eddc5c751a6b3b44b73ce07cc370c
Fabrizio Panato <fbzpanatto@hotmail.com>
2025-08-10 13:35:10 -0300

    Staging...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadComponent: () => import('./components/home/home').then(m => m.Home) },
+  { path: 'import-data', loadComponent: () => import('./components/import-data/import-data').then(m => m.ImportData) },
+  { path: 'order', loadComponent: () => import('./components/order/order').then(m => m.Order) },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
---
+++ src/app/components/import-data/import-data.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-import-data',
+  imports: [],
+  templateUrl: './import-data.html',
+  styleUrl: './import-data.scss'
+})
+export class ImportData {
+
+}
